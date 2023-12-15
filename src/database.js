d3c65c0ad8d8da8642cfe96c1a92a9a231cfed5f
Eme Hache <luisalfonsodsg@outlook.com>
2023-12-15 13:00:02 -0500

    fix: connection string in production

--- src/database.js
+++ src/database.js
@@ -3,13 +3,10 @@ import mongoose from 'mongoose'
 let connectionString
 
 if (process.env.ENV === 'production') {
-    connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.CLUSTER}.mongodb.net/${process.env.DB}`
+    connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.CLUSTER}.mongodb.net/`
 } else {
     connectionString = `mongodb://${process.env.HOST}:${process.env.PORT}/${process.env.DB}`
 }
-
-console.log(process.env.ENV)
-console.log(connectionString)
 
 mongoose.connect(connectionString)
     .then(db => console.log('DB connected'))
