be15e8b183efeca1d0afd083312a7a588dca075e
Eme Hache <luisalfonsodsg@outlook.com>
2023-12-14 22:24:21 -0500

    feat: add env variables

--- src/database.js
+++ src/database.js
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose'
 
-mongoose.connect('mongodb://localhost/companydb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+let connectionString
+
+if (process.env.NODE_ENV === 'production') {
+    connectionString = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.CLUSTER}.mongodb.net/${process.env.DB}`
+} else {
+    connectionString = `mongodb://${process.env.HOST}:${process.env.PORT}/${process.env.DB}`
+}
+
+mongoose.connect(connectionString)
     .then(db => console.log('DB connected'))
     .catch(err => console.log(err))
